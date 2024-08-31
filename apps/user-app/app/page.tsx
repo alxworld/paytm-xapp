23f18cb1d8461b219329061002192f4e2c22f08d
alxworld <jsalex@gmail.com>
2024-08-31 19:50:25 +0530

    sample prisma client testing in home page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,5 +1,9 @@
 import './globals.css'
 
+import { PrismaClient } from '@repo/db/client'
+
+const client = new PrismaClient()
+
 export default function Home() {
   return <h1 className="text-3xl font-bold">Hi From Home Page</h1>
 }
