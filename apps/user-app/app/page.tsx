cf0c3add07547aea64f7ac708512795d39983663
alxworld <jsalex@gmail.com>
2024-09-01 22:54:02 +0530

    Next Auth Completed

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
 import './globals.css'
+import { Appbar } from '@repo/ui/Appbar'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
-import { PrismaClient } from '@repo/db/client'
-
-const client = new PrismaClient()
+//import { PrismaClient } from '@repo/db/client'
+// const client = new PrismaClient()
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hi From Home Page</h1>
+  const session = useSession()
+  return (
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+      <h1 className="text-3xl font-bold">Hi From Home Page</h1>
+    </div>
+  )
 }
